f9ecedf3f11c4100413151db26df2be66d7e4a9e
pallard <pallard.git@pm.me>
2026-04-19 08:26:47 -0400

    added some alias in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,13 @@ zoxide init fish | source
 fzf --fish | source
 mise activate fish | source
 
+alias c "clear"
+alias e "exit"
+
 alias cd "z"
 alias ls "eza -al"
 alias lt 'eza -aT --color=always --group-directories-first --icons'
+
+alias lg "lazygit"
+
+alias ta "tmux attach"
